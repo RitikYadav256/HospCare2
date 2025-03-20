3716456f0181a96b0072df80136282a064cb729f
RitikYadav256 <118496635+RitikYadav256@users.noreply.github.com>
2025-03-20 16:49:37 +0530

    change

--- Hosp/utils/MangeData.js
+++ Hosp/utils/MangeData.js
@@ -1,27 +1,31 @@
-const { MongoClient } = require("mongodb");
-
-const mongoUrl = "mongodb+srv://ritik:8287617808@ritikyadav.9q3yt.mongodb.net/?retryWrites=true&w=majority&appName=RitikYadav";
+const { MongoClient } = require('mongodb');
 
 let _db;
-const mongoConnect = (callback) => {
-  
-  MongoClient.connect(mongoUrl)
-    .then(client => {
-      console.log("Connected to MongoDB");
-      callback(client);
-      _db=client.db('Hosp_db');
-    })
-    .catch(error => {
-      console.error("Error while connecting to MongoDB:", error);
+const mongoUrl = process.env.MONGO_URI || "mongodb+srv://ritik:8287617808@ritikyadav.9q3yt.mongodb.net/?retryWrites=true&w=majority&appName=RitikYadav";
+
+const mongoConnect = async (callback) => {
+  try {
+    const client = await MongoClient.connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      tls: true,  // Ensure secure connection
     });
+
+    console.log("✅ Connected to MongoDB");
+    _db = client.db("Hosp_db");
+
+    if (callback) callback(client);
+  } catch (error) {
+    console.error("❌ Error while connecting to MongoDB:", error);
+  }
 };
 
-const getDB=()=>{
-  if(!_db){
-    throw new error("not connected");
+const getDB = () => {
+  if (!_db) {
+    throw new Error("❌ Not connected to MongoDB!");
   }
   return _db;
-}
+};
 
 exports.mongoConnect = mongoConnect;
-exports.getDB=getDB;
+exports.getDB = getDB;
